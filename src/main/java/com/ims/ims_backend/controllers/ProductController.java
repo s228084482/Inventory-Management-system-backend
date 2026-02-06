21dd1d3229d2b99f5ad78f11bc9ce6d40e26ec6a
Philasande <s228084482@mandela.ac.za>
2026-02-06 22:38:36 +0200

    Added some product backend usecase.

--- src/main/java/com/ims/ims_backend/controllers/ProductController.java
+++ src/main/java/com/ims/ims_backend/controllers/ProductController.java
@@ -1,12 +1,19 @@
 package com.ims.ims_backend.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+    @PostMapping("/save")
+    public String saveProduct(@RequestParam String name, @RequestParam float price, @RequestParam long qty,@RequestParam String description){
 
+        return "Ok";
+    }
+    @PostMapping("/saveProductInfo")
+    public String saveProductInfoData(){
+
+        return "Ok";
+    }
 }
